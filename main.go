524f93cf561141199f1b35667720fa1f39b26cff
Marvin Luckianto <marvnluck@gmail.com>
2024-02-27 17:48:05 +0700

    fix: removed REST API
    setting up for cron-job implementation first

--- main.go
+++ main.go
@@ -1,50 +1,20 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
-	"os"
-
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"get-bensin/scraper"
+	"get-bensin/types"
+	"get-bensin/util"
 )
 
-type Fuel struct {
-	ID        uint   `json:"id"`
-	FuelName  string `json:"fuel_name"`
-	FuelPrice string `json:"fuel_price"`
-}
+var (
+	fuels []types.Fuel
+)
 
 func main() {
-	r := gin.Default()
+	scraper.ScrapePertamina(&fuels)
+	scraper.ScrapeShell(&fuels)
 
-	// Define your endpoint to handle the GET request
-	r.GET("/fuels", func(c *gin.Context) {
-		// Read fuel data from JSON file
-		fuels, err := readFuelsFromJSON("fuels.json")
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to read fuel data"})
-			return
-		}
-
-		// Return fuels as JSON response
-		c.JSON(http.StatusOK, fuels)
-	})
-	r.Run()
-}
-
-func readFuelsFromJSON(filename string) ([]Fuel, error) {
-	// Open the JSON file
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	// Decode JSON data into a slice of Fuel structs
-	var fuels []Fuel
-	if err := json.NewDecoder(file).Decode(&fuels); err != nil {
-		return nil, err
-	}
-
-	return fuels, nil
+	fmt.Println(&fuels)
+	util.WriteJSON(&fuels)
 }
