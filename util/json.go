8501834d0e7b14a035f9061e9dbe9073333644ce
Marvin Luckianto <marvnluck@gmail.com>
2024-02-27 17:46:18 +0700

    feat: added json formatting and writing functions

---
+++ util/json.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"get-bensin/types"
+)
+
+func WriteJSON(data *[]types.Fuel) {
+	file, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		fmt.Println("Unable to create JSON file")
+		return
+	}
+	_ = os.WriteFile("fuels.json", file, 0644)
+}
+
+func ToIDR(str string) int {
+	if str == "-" || str == "N/A" {
+		return 0
+	}
+	trimmed := strings.TrimPrefix(str, "IDR ")
+	trimmed = strings.TrimPrefix(trimmed, "Rp")
+	trimmed = strings.ReplaceAll(trimmed, ",", "")
+	num, err := strconv.Atoi(trimmed)
+	if err != nil {
+		log.Fatalln("Error:", err)
+		return -1
+	}
+	return num
+}
